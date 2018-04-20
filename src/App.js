e7582825d1d659cf7bfd31ea45b0c839ad85818c
Dave Martinez <dkm.coder@gmail.com>
2018-04-20 17:11:40 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './socketCommunication'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Teltris</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
