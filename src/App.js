d9fc8041a4c01495b47588ccb50ff06890f55e4f
Dave Martinez <dkm.coder@gmail.com>
2018-04-20 19:55:25 +0200

    Feat: Add keyboard event listener to the main app component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,28 @@ import Board from './containers/board';
 import './socketCommunication'
 
 class App extends Component {
+
+  handleKeyPress = (event) => {
+    if(event.key === 'ArrowLeft'){
+      console.log('Going left...')
+    }
+    if(event.key === 'ArrowRight'){
+      console.log('Going right...')
+    }
+    if(event.key === 'ArrowUp'){
+      console.log('Going up...')
+    }
+    if(event.key === 'ArrowDown'){
+      console.log('Going down...')
+    }
+    if(event.key === ' '){
+      console.log('Spacebar')
+    }
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" onKeyDown={this.handleKeyPress} tabIndex="0">
         <Board />
         <Board />
       </div>
