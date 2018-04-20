54c9cfa48f99b4efb167fd6b8f984ae9f80f8e6d
Dave Martinez <dkm.coder@gmail.com>
2018-04-20 19:57:40 +0200

    Chore: Get a reference to the 2D context of the canvas element

--- src/containers/board.js
+++ src/containers/board.js
@@ -3,9 +3,13 @@ import './board.css';
 import { socketHandler, socket } from '../socketCommunication';
 
 class Board extends Component {
+  componentDidMount() {
+    this.tetrisCanvasContext = this.refs.tetrisCanvas.getContext('2d');
+  }
+
   render() {
     return (
-      <canvas id="tetris-canvas" width="240" height="400"></canvas>
+      <canvas width="240" height="400" ref="tetrisCanvas"></canvas>
     );
   }
 }
